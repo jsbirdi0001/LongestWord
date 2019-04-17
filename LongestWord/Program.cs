1b05d991b749558f729735b996d82c3e7465c9a5
visualstudio <visualstudio@122-PC04>
2019-04-17 10:36:31 -0400

    Longest Word

--- LongestWord/Program.cs
+++ LongestWord/Program.cs
@@ -10,6 +10,32 @@ namespace LongestWord
     {
         static void Main(string[] args)
         {
+            Program a = new Program();
+            Console.WriteLine("The longest word is: ");
+            a.MyFunctionA("Ada Lovelace wrote the first algorithm designed for procession by an Analytical Engine");
+        }
+
+        public void MyFunctionA(string output)
+        {
+            string[] words = output.Split(' ');
+            string b;
+            int wordArrayLength = words.Length;
+            int x = 0;
+            foreach(var word in words)
+            {
+                string a;
+                foreach(var word2 in words)
+                {
+                    if(word2.Length > word.Length)
+                    {
+                        a = word2;
+                        Console.WriteLine("{0} : This is longest word now",a);
+                    }
+                }
+                
+            }
+            Console.ReadLine();
+            
         }
     }
 }
